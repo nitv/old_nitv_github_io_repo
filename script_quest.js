1807eb3afa58b1ae51c82fe2a77288f4ecc36152
Nitin Verma <Nitin Verma>
2016-04-08 14:33:35 -0500

    console message

--- script_quest.js
+++ script_quest.js
@@ -53,6 +53,7 @@ $.ajax({
 	
        console.log(combos);    
        comboIdArray = create_unique_random_array(5, 0, 4);
+       console.log(comboIdArray);
        //var comboId = Math.floor(Math.random()*$(json).length);
        //$("#app_image").attr('src', combos[comboId].appURI);
        //app.onload = getAppImageDimensions;
