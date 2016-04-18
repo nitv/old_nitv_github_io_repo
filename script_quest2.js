33c8049faaa5040a7437feb0a5cca312b998fa25
Nitin Verma <Nitin Verma>
2016-04-18 15:24:33 -0500

    dev

--- script_quest2.js
+++ script_quest2.js
@@ -114,7 +114,7 @@ function askQuestions()
             quesForm.appendChild(document.createElement("br"));
         }
         //question.appendChild(answers);
-        question.appendChild(document.createElement("hr"));
+        quesForm.appendChild(document.createElement("hr"));
     }
     var submitBtn = document.createElement("input");
     submitBtn.type = "submit";
