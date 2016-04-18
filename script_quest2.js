5b6bb2e4b787d395cbc0403bceff008469cea82b
Nitin Verma <Nitin Verma>
2016-04-17 21:03:04 -0500

    bugfix

--- script_quest2.js
+++ script_quest2.js
@@ -9,6 +9,75 @@ var introContainer;
 var formContainer;
 var state = 0;
 var numStates = 3;
+var combos = [];
+var comboIdArray = [];
+var currentComboIdIdx = 0;
+
+$.ajax({
+    type: "GET",
+    url: "combosquestions.txt",
+    dataType: "json",
+    success: function(json) {
+        for (i=0; i < $(json).length; i++) {
+            var combo = {};
+            //console.log($(json)[i].id, $(json)[i].appURI, $(json)[i].adURI, $(json)[i].adpos);
+            combo.id = $(json)[i].combo;
+            combo.appURI = $(json)[i].images.appURI;
+            combo.adURI = $(json)[i].images.adURI;
+            combo.adpos = $(json)[i].images.adpos;
+            combo.questions = [];
+            for (j=0; j < $(json)[i].questions.length; j++){
+                var question = {};
+                question.id = $(json)[i].questions[j].id;
+                question.ques = $(json)[i].questions[j].ques;
+                question.choices = $(json)[i].questions[j].choices;
+                combo.questions.push(question);
+            }
+            combos.push(combo);
+        }
+    
+        console.log(combos);    
+        comboIdArray = create_unique_random_array(maxScreens, 0, maxScreens - 1);
+        console.log(comboIdArray);
+    }
+})
+
+// RETURNS PSEUDO-RANDOM NUMBER IN RANGE min...max
+function random_number(min,max) {
+    return (Math.round((max-min) * Math.random() + min));
+}
+
+function create_unique_random_array(num_elements, min, max) {
+    var temp, nums = new Array;
+
+    for (var element=0; element<num_elements; element++) {
+        //IMPORTANT: DON'T FORGET THE SEMI-COLON AT THE END
+        while((temp=number_found(random_number(min,max),nums))==-1);
+        nums[element] = temp;
+    }
+
+    return (nums);
+}
+
+function number_found (random_number, number_array) {
+    for (var element=0; element<number_array.length; element++) {
+        if (random_number==number_array[element]) {
+            return (-1);
+        }
+    }
+
+    return (random_number);
+}
+
+function getNextImage()
+{
+    comboId = comboIdArray[currentComboIdIdx];
+    currentComboIdIdx = (currentComboIdIdx + 1) % 5;
+    $("#app_image").attr('src', combos[comboId].appURI);
+    app.onload = getAppImageDimensions;
+    $("#ad_image").attr('src', combos[comboId].adURI).attr('class', combos[comboId].adpos);
+}
+
 
 function init()
 {
